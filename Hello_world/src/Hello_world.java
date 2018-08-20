f6da49d432da45e77f5831ffde7f8973eb9a86e3
APCS3rd <APCS3rd@MHS-Dreyer-17>
2018-08-20 10:43:20 -0700

    First commit to Github

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,12 @@
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author Shoh Mollenkamp
+ * @version 8/17/2018
+ */
+public class Hello_world {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! From <Shoh Mollenkamp>"); //Display the string
+	}
+}
